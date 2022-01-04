bee57fc58c795e9a4d37f458b851b5794b3716c7
efe3535 <a.efe.akyazi@gmail.com>
2022-01-04 19:53:42 +0300

    fix a little bug, shit

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
         io::stdin().read_line(&mut replace_with).expect("Failed to get input.");
     } else {
         if args[1] != "--help" && args[1] != "-h" {
-            replace_with.push_str(&args[1].trim());
+            replace_with.push_str(&args[1]);
         } 
         if args[1].starts_with("--") && args[1] != "--help" && args[1] != "-h" {
             println!("bad usage, see {} --help", args[0]);
@@ -24,12 +24,6 @@ fn main() {
         }
     }
     
-    /*
-    else {
-        replace_with = String::from(args()[1]);
-    }
-    */
-    
     let joke = String::from("I'd just like to interject for a moment.  What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component
 of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.
 Many computer users run a modified version of the GNU system every day, without realizing it.  Through a peculiar turn of events, the version of GNU
@@ -38,6 +32,6 @@ There really is a Linux, and these people are using it, but it is just a part of
 that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself;
 it can only function in the context of a complete operating system.  Linux is normally used in combination with the GNU operating system: the whole system
 is basically GNU with Linux added, or GNU/Linux.  All the so-called Linux distributions are really distributions of GNU/Linux."); 
-    let res = str::replace(&joke, "Linux", &replace_with);
+    let res = str::replace(&joke, "Linux", &replace_with.trim());
     println!("{}", res);
 }
